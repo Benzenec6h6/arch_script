c6f8d8c043b7469a09695d4e1a8ef897a29b9059
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2025-07-21 23:18:34 +0900

    8,13,19 delete --now

--- chroot/06_enable.sh
+++ chroot/06_enable.sh
@@ -2,26 +2,24 @@
 set -euo pipefail
 source ./env.sh
 
-systemctl --user enable --now pipewire pipewire-pulse wireplumber
-
 usermod -aG video,audio "$USERNAME"
 
 if ! $is_vm; then
-  systemctl enable --now bluetooth cups tlp
+  systemctl enable bluetooth cups tlp
   usermod -aG input "$USERNAME"
 fi
 
 # libvirt
-systemctl enable --now libvirtd
+systemctl enable libvirtd
 sed -Ei 's|^#(unix_sock_group = )"libvirt"|\1"libvirt"|' /etc/libvirt/libvirtd.conf
 sed -Ei 's|^#(unix_sock_rw_perms = )"0770"|\1"0770"|' /etc/libvirt/libvirtd.conf
 usermod -aG libvirt "$USERNAME"
 
 # docker
-systemctl enable --now docker containerd
+systemctl enable docker containerd
 usermod -aG docker "$USERNAME"
-
-fc-cache -fv
 
 # winetricks
 sudo -u "$USERNAME" winetricks -q cjkfonts || true
+
+fc-cache -fv
